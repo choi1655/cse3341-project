9e6182452583705c960e943e05473226f302ab8e
johnchoi96 <johnchoi1003@icloud.com>
2021-09-22 23:02:39 -0400

    Main class refactor

--- Main.java
+++ Main.java
@@ -1,23 +1,23 @@
 class Main {
 	public static void main(String[] args) {
 		// Initialize the scanner with the input file
-		Scanner S = new Scanner(args[0]);
+		Scanner s = new Scanner(args[0]);
 
 		// Print the token stream
-		while (S.currentToken() != Core.EOF && S.currentToken() != Core.ERROR) {
-			// Pring the current token, with any extra data needed
-			System.out.print(S.currentToken());
-			if (S.currentToken() == Core.ID) {
-				String value = S.getID();
+		while (s.currentToken() != Core.EOF && s.currentToken() != Core.ERROR) {
+			// Print the current token, with any extra data needed
+			System.out.print(s.currentToken());
+			if (s.currentToken() == Core.ID) {
+				String value = s.getID();
 				System.out.print("[" + value + "]");
-			} else if (S.currentToken() == Core.CONST) {
-				int value = S.getCONST();
+			} else if (s.currentToken() == Core.CONST) {
+				int value = s.getCONST();
 				System.out.print("[" + value + "]");
 			}
 			System.out.print("\n");
 
 			// Advance to the next token
-			S.nextToken();
+			s.nextToken();
 		}
 	}
 }
